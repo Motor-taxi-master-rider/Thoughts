d198ca9d012b97a347b6394a72cd2823c4310185
摩的大飚客 <gflame42@gmail.com>
2017-02-08 00:28:54 +0800

    20170208

---
+++ ThinkStats/Quiz/3-8.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.append('\Python\ThinkStats')
+import urllib.request
+import Pmf
+
+results = 'http://coolrunning.com/results/10/ma/Apr25_27thAn_set1.shtml'
+
+def ReadResults(url=results):
+    results = []
+    conn = urllib.request.urlopen(url)
+    for line in conn.fp:
+        t = CleanLine(line)
+        if t:
+            results.append(t)
+    return results
+
+def CleanLine(line):
+    t = line.split()
+    if len(t) < 11:
+        return None
+
+    place, divtot, div, gun, net, pace, name, age, sex, race, loc = t[0:11]
+
+    if not '/'.encode() in divtot:
+        return None
+
+    for time in [gun, net, pace]:
+        if ':'.encode() not in time:
+            return None
+
+    return place.decode(), divtot.decode(), div.decode(), gun.decode(), net.decode(), pace.decode(),name.decode(), age.decode(), sex.decode(), race.decode(), loc.decode()
+
+def main():
+    results = ReadResults()
+    print(results)
+
+if __name__ == '__main__':
+    main()
