8fd7cd753125670f71f01f943c63845f30e576be
chu060 <charlie.hu@pwc.com>
2017-02-10 17:32:06 +0800

    20170210

--- ThinkStats/Quiz/4-4.py
+++ ThinkStats/Quiz/4-4.py
@@ -3,3 +3,12 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import random
 import numpy as np
+
+lst = []
+for _i in range(1000):
+    lst.append(random.paretovariate(1.7) * 100)
+mean = np.mean(lst)
+print(mean)
+print(np.max(lst))
+print(np.min(lst))
+print((len(list(filter(lambda x: x <mean, lst))))/len(lst))
