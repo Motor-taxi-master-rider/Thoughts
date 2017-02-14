3ad10c47a1a3644c6730cb2108dc8873b749e2e0
chu060 <charlie.hu@pwc.com>
2017-02-14 17:20:09 +0800

    20170214

--- ThinkStats/Quiz/4-9.py
+++ ThinkStats/Quiz/4-9.py
@@ -3,4 +3,24 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import numpy as np
 
-def Sample():
+def Sample(u = 0, v = 1, n):
+    return np.random.normal(u, v, n)
+
+def Samples(n = 6, ):
+    list = []
+    for _i in range(1000):
+        list.append(Sample())
+    return list
+
+zipped = []
+for sample in Samples():
+    if(not zipped):
+        zipped = sample
+    else:
+        zipped = zip(zipped, sample)
+print(zipped)
+
+for sample in zipped:
+    list = []
+    list.append(np.mean(sample))
+#print(list)
