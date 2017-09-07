d91e52559efb3904c669594cb26c554fa0b8af0a
Zhiyu Hu <zhiyu.hu@ericsson.com>
2017-09-07 17:43:57 +0800

    :memo: add coroutine notebook

--- Script/tornado_coroutine.py
+++ Script/tornado_coroutine.py
@@ -3,6 +3,7 @@ import tornado.ioloop
 import tornado.web
 import tornado.gen
 
+
 class BadStupidHandler(tornado.web.RequestHandler):
     def get(self):
         for i in range(20):
@@ -10,19 +11,20 @@ class BadStupidHandler(tornado.web.RequestHandler):
             self.flush()
             time.sleep(0.5)
 
+
 class GoodStupidHandler(tornado.web.RequestHandler):
     @tornado.gen.coroutine
     def get(self):
         for i in range(20):
             self.write('{}<br>'.format(i))
             self.flush()
-            yield time.sleep(0.5)
+            yield tornado.gen.sleep(0.5)
 
 
 if __name__ == "__main__":
-    app=tornado.web.Application([
-        (r'/bad',BadStupidHandler),
+    app = tornado.web.Application([
+        (r'/bad', BadStupidHandler),
         (r'/good', GoodStupidHandler)
     ])
-    app.listen(8888)
+    app.listen(8001)
     tornado.ioloop.IOLoop.current().start()
