a38657a913fe58e30532fa14a5e7afe0de32008f
摩的大飚客 <hzy1324@qq.com>
2018-04-23 17:58:12 +0800

    :new: new scripts for max sub array algorithm

---
+++ Script/algorithm/max_sub_array.py
@@ -0,0 +1,22 @@
+def max_sub_array(array):
+    current_start = max_end = max_start = -1
+    max_sum = sum_current = 0
+    for index, value in enumerate(array):
+        if sum_current + value < 0:
+            current_start = index + 1
+            sum_current = 0
+        else:
+            sum_current = sum_current + value
+        if sum_current >= max_sum:
+            max_start = current_start
+            max_end = index
+            max_sum = sum_current
+
+    return max_sum, max_start, max_end
+
+
+if __name__ == '__main__':
+    test = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    max_value, start, end = max_sub_array(test)
+    assert test[start:end + 1] == [4, -1, 2, 1]
+    assert max_value == 6
