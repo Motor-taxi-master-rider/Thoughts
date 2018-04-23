a866859447849040da4f367d98b09f0db2832732
摩的大飚客 <hzy1324@qq.com>
2018-04-23 18:07:24 +0800

    :wrench: fix algorithm and add unittest

--- Script/algorithm/max_sub_array.py
+++ Script/algorithm/max_sub_array.py
@@ -1,5 +1,5 @@
 def max_sub_array(array):
-    current_start = max_end = max_start = -1
+    current_start = max_end = max_start = None
     max_sum = sum_current = 0
     for index, value in enumerate(array):
         if sum_current + value < 0:
@@ -7,7 +7,7 @@ def max_sub_array(array):
             sum_current = 0
         else:
             sum_current = sum_current + value
-        if sum_current >= max_sum:
+        if sum_current > max_sum:
             max_start = current_start
             max_end = index
             max_sum = sum_current
@@ -20,3 +20,13 @@ if __name__ == '__main__':
     max_value, start, end = max_sub_array(test)
     assert test[start:end + 1] == [4, -1, 2, 1]
     assert max_value == 6
+
+    test = [-2, 1, -3]
+    max_value, start, end = max_sub_array(test)
+    assert test[start:end + 1] == [1]
+    assert max_value == 1
+
+    test = [-2, -1, -3]
+    max_value, start, end = max_sub_array(test)
+    assert start is None and end is None
+    assert max_value == 0
