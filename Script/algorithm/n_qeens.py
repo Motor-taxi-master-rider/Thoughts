98f15be7b889e6941a23aa027c7925e49f8a0f49
摩的大飚客 <gflame42@gmail.com>
2018-05-09 22:21:57 +0800

    :memo: new algorithm: n queens

---
+++ Script/algorithm/n_qeens.py
@@ -0,0 +1,52 @@
+def n_queens(n):
+    array = []
+    result = []
+
+    def place_queen(col):
+        nonlocal n, array
+        if col == n:
+            return True
+        for row in range(n):
+            array.append((row, col))
+            if is_safe(row, col) and place_queen(col + 1):
+                result.append(array)
+            array.pop()
+        return False
+
+    def is_safe(row, col):
+        for r, c in array[:-1]:
+            if r == row:
+                return False
+            if abs(r - row) == abs(c - col):
+                return False
+        return True
+
+    place_queen(0)
+    return result
+
+
+def n_queens_iter(n):
+    array = [1] * n
+    result = []
+
+    def place_queen(array, col):
+        nonlocal n, result
+        if col == n:
+            result.append(array)
+            return
+        for row in range(n):
+            array[col] = row
+            flag = True
+            for c, r in enumerate(array[:col]):
+                if r == row or abs(r - row) == abs(c - col):
+                    flag = False
+                    break
+            if flag:
+                place_queen(array, col + 1)
+    place_queen(array, 0)
+    return result
+
+
+if __name__ == '__main__':
+    assert len(n_queens(8) == 92
+    assert len(n_queens_iter(8)) == 92
