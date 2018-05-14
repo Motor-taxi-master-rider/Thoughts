075b708c4bc4a697bf4c597d44f59ae5d9694bb4
摩的大飚客 <gflame42@gmail.com>
2018-05-14 23:29:27 +0800

    :memo: new algorithm: check BST

---
+++ Script/algorithm/basic_algorithm/check_BST.py
@@ -0,0 +1,30 @@
+import sys
+import math
+sys.path.append('..')
+from algorithm.data_structure.Tree import build_tree_from_list
+
+
+def check_BST(root, low, high):
+    """check a binary tree whether is BST or not""
+    if not root:
+        return True
+    root_value = root.value
+    if not low <= root_value <= high:
+        return False
+    if not check_BST(root.left, low, root_value):
+        return False
+    return check_BST(root.right, root_value, high)
+
+
+if __name__ == '__main__':
+    lst = [20, None, 50, None, None, 40, 75, None, None, None, None, 21]
+    test = build_tree_from_list(lst)
+    assert check_BST(test, -math.inf, math.inf) == True
+
+    lst = [20, None, 50, None, None, 40, 75, None, None, None, None, 20]
+    test = build_tree_from_list(lst)
+    assert check_BST(test, -math.inf, math.inf) == True
+
+    lst = [20, None, 50, None, None, 40, 75, None, None, None, None, 19]
+    test = build_tree_from_list(lst)
+    assert check_BST(test, -math.inf, math.inf) == False
