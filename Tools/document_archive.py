6b15899a712edea28f24413488291bd97966e011
摩的大飚客 <hzy1324@qq.com>
2018-08-05 19:04:34 +0800

    :construction: new version of document archive for MindPalace

--- Tools/document_archive.py
+++ Tools/document_archive.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import re
 
@@ -27,32 +28,36 @@ def document_archive():
 def _parse_title(title: str) -> dict:
     data = {'priority': 0}
     if title.startswith('#'):
-        data['category'] = 'STERM'
+        data['category'] = 'short term'
+        data['tags'] = ['to do']
         if title.startswith('##'):
             data['priority'] = 2
         else:
             data['priority'] = 3
         data['theme'] = STERM_REG.sub('', title).strip()
     elif title.startswith('`'):
-        data['category'] = 'LTERM'
+        data['category'] = 'long term'
         data['theme'] = LTERM_REG.findall(title)[0].strip()
     elif title.startswith('**'):
-        data['category'] = 'INTEREST'
+        data['category'] = 'short term'
         high_light = INTERST_REG.findall(title)
         data['theme'] = high_light.pop(0).strip()
+        data['tags'] = ['reviewed', 'impressive', 'cache']
         if high_light:
             data['comment'] = high_light
     elif title.startswith('_'):
-        data['category'] = 'REVIEWED'
+        data['category'] = 'short term'
         data['theme'] = REVIEW_REG.findall(title)[0].strip()
         high_light = INTERST_REG.findall(title)
+        data['tags'] = ['reviewed', 'cache']
         if high_light:
             data['comment'] = high_light
     elif title.startswith('~~'):
-        data['category'] = 'FLIP'
+        data['category'] = 'flip'
         data['theme'] = FLIP_REG.findall(title)[0].strip()
     else:
-        data['category'] = 'STERM'
+        data['category'] = 'short term'
         data['priority'] = 0
         data['theme'] = title.strip()
+    data['update_at'] = datetime.datetime.utcnow()
     return data
